80f431e8b9df3266d77fa1b1d7240dcd8237dc76
yanataka60 <92910500+yanataka60@users.noreply.github.com>
2023-10-23 14:19:06 +0900

    Add files via upload

--- Arduino/X1_SD/X1_SD.ino
+++ Arduino/X1_SD/X1_SD.ino
@@ -1,15 +1,3 @@
-// 2022. 1.24 ファイルネームの後ろの20h詰めを0dhに修正するための処理をArduino側からMZ-80K側に修正
-//           比較演算子の記述を見直し
-// 2022. 1.25 各コマンド受信時のdelay()を廃止
-// 2022. 1.26 FDコマンドでロード可能なファイル種類コードは0x01のみとしていた制限を撤廃
-// 2022. 1.29 FDPコマンドのbug修正
-// 2022. 1.30 FDLコマンド仕様変更 FDL A～Zの場合、ファイル名先頭一文字を比較して一致したものだけを出力
-// 2022. 1.31 FDLコマンド仕様変更 FDL xの場合、ファイル名先頭一文字を比較して一致したものだけを出力
-//            Bキーで前の20件を表示
-// 2022. 2. 2 DOSファイル名がアルファベット小文字でもFDL xで検索できるように修正
-// 2022. 2. 4 MZ-1200対策　初期化時にdelay(1000)を追加
-// 2022. 2. 8 FDLコマンド仕様変更 FDL xの場合、ファイル名先頭1文字～32文字までに拡張
-// 2023. 6.19 MZ-2000_SDの起動方式追加によりBOOT LOADER読み込みを追加。MZ-80K_SDには影響なし。
 //
 #include "SdFat.h"
 #include <SPI.h>
@@ -19,7 +7,8 @@ char m_name[40];
 byte s_data[260];
 char f_name[40];
 char c_name[40];
-char new_name[40];
+char name1[14];
+char name2[4];
 
 #define CABLESELECTPIN  (10)
 #define CHKPIN          (15)
@@ -40,7 +29,7 @@ char new_name[40];
 boolean eflg;
 
 void setup(){
-  Serial.begin(9600);
+////  Serial.begin(9600);
 // CS=pin10
 // pin10 output
 
@@ -71,19 +60,16 @@ void setup(){
   digitalWrite(PB7PIN,LOW);
   digitalWrite(FLGPIN,LOW);
 
-// 2022. 2. 4 MZ-1200対策
-//  delay(1500);
-
   // SD初期化
   if( !SD.begin(CABLESELECTPIN,8) )
   {
-    Serial.println("Failed : SD.begin");
+////    Serial.println("Failed : SD.begin");
     eflg = true;
   } else {
-    Serial.println("OK : SD.begin");
+////    Serial.println("OK : SD.begin");
     eflg = false;
   }
-  Serial.println("START");
+////  Serial.println("START");
 }
 
 //4BIT受信
@@ -141,9 +127,9 @@ char upper(char c){
 }
 
 //ファイル名の最後が「.x1t」でなければ付加
-void addmzt(char *f_name){
+void addx1t(char *f_name){
   unsigned int lp1=0;
-  while (f_name[lp1] != 0x0D){
+  while (f_name[lp1] != 0x0D && f_name[lp1] != 0x00){
     lp1++;
   }
   if (f_name[lp1-4]!='.' ||
@@ -160,92 +146,17 @@ void addmzt(char *f_name){
   f_name[lp1] = 0x00;
 }
 
-//SDカードにSAVE
-void f_save(void){
-char p_name[20];
-
-//保存ファイルネーム取得
-  for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-    f_name[lp1] = rcv1byte();
-  }
-  addmzt(f_name);
-//プログラムネーム取得
-  for (unsigned int lp1 = 0;lp1 <= 16;lp1++){
-    p_name[lp1] = rcv1byte();
-  }
-  p_name[15] =0x0D;
-  p_name[16] =0x00;
-//スタートアドレス取得
-  int s_adrs1 = rcv1byte();
-  int s_adrs2 = rcv1byte();
-//スタートアドレス算出
-  unsigned int s_adrs = s_adrs1+s_adrs2*256;
-//エンドアドレス取得
-  int e_adrs1 = rcv1byte();
-  int e_adrs2 = rcv1byte();
-//エンドアドレス算出
-  unsigned int e_adrs = e_adrs1+e_adrs2*256;
-//実行アドレス取得
-  int g_adrs1 = rcv1byte();
-  int g_adrs2 = rcv1byte();
-//実行アドレス算出
-  unsigned int g_adrs = g_adrs1+g_adrs2*256;
-//ファイルサイズ算出
-  unsigned int f_length = e_adrs - s_adrs + 1;
-  unsigned int f_length1 = f_length % 256;
-  unsigned int f_length2 = f_length / 256;
-//ファイルが存在すればdelete
-  if (SD.exists(f_name) == true){
-    SD.remove(f_name);
-  }
-//ファイルオープン
-  File file = SD.open( f_name, FILE_WRITE );
-  if( true == file ){
-//状態コード送信(OK)
-    snd1byte(0x00);
-//ファイルモード設定(01)
-    file.write(char(0x01));
-//プログラムネーム
-    file.write(p_name);
-    file.write(char(0x00));
-//ファイルサイズ
-    file.write(f_length1);
-    file.write(f_length2);
-//スタートアドレス
-    file.write(s_adrs1);
-    file.write(s_adrs2);
-//実行アドレス
-    file.write(g_adrs1);
-    file.write(g_adrs2);
-//7Fまで00埋め
-    for (unsigned int lp1 = 0;lp1 <= 103;lp1++){
-      file.write(char(0x00));
-    }
-//実データ
-    long lp1 = 0;
-    while (lp1 <= f_length-1){
-      int i=0;
-      while(i<=255 && lp1<=f_length-1){
-        s_data[i]=rcv1byte();
-        i++;
-        lp1++;
-      }
-      file.write(s_data,i);
-    }
-    file.close();
-   } else {
-//状態コード送信(ERROR)
-    snd1byte(0xF1);
-  }
-}
-
 //SDカードから読込
 void f_load(void){
 //ファイルネーム取得
   for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
     f_name[lp1] = rcv1byte();
+////  Serial.print(f_name[lp1],HEX);
   }
-  addmzt(f_name);
+////  Serial.println("");
+////  Serial.println(f_name);
+  addx1t(f_name);
+////  Serial.println(f_name);
 //ファイルが存在しなければERROR
   if (SD.exists(f_name) == true){
 //ファイルオープン
@@ -280,16 +191,6 @@ void f_load(void){
         unsigned int g_adrs = g_adrs1*256+g_adrs2;
         snd1byte(g_adrs2);
         snd1byte(g_adrs1);
-////  Serial.print(f_length2,HEX);
-////  Serial.println(f_length1,HEX);
-////  Serial.println(f_length,HEX);
-////  Serial.print(s_adrs2,HEX);
-////  Serial.println(s_adrs1,HEX);
-////  Serial.println(s_adrs,HEX);
-////  Serial.print(g_adrs2,HEX);
-////  Serial.println(g_adrs1,HEX);
-////  Serial.println(g_adrs,HEX);
-//        file.seek(32);
 
         for (unsigned int lp1 = 0;lp1 <= 7;lp1++){
           wk1 = file.read();
@@ -318,51 +219,6 @@ void f_load(void){
   }
 }
 
-//ASTART 指定されたファイルをファイル名「0000.mzt」としてコピー
-void astart(void){
-char w_name[]="0000.mzt";
-
-//ファイルネーム取得
-  for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-    f_name[lp1] = rcv1byte();
-  }
-  addmzt(f_name);
-//ファイルが存在しなければERROR
-  if (SD.exists(f_name) == true){
-//0000.mztが存在すればdelete
-    if (SD.exists(w_name) == true){
-      SD.remove(w_name);
-    }
-//ファイルオープン
-    File file_r = SD.open( f_name, FILE_READ );
-    File file_w = SD.open( w_name, FILE_WRITE );
-      if( true == file_r ){
-//実データ
-        unsigned int f_length = file_r.size();
-        long lp1 = 0;
-        while (lp1 <= f_length-1){
-          int i=0;
-          while(i<=255 && lp1<=f_length-1){
-            s_data[i]=file_r.read();
-            i++;
-            lp1++;
-          }
-          file_w.write(s_data,i);
-        }
-        file_w.close();
-        file_r.close();
-//状態コード送信(OK)
-        snd1byte(0x00);
-      } else {
-//状態コード送信(ERROR)
-      snd1byte(0xF1);
-    }
-  } else {
-//状態コード送信(ERROR)
-    snd1byte(0xF1);
-  }  
-}
-
 // SD-CARDのFILELIST
 void dirlist(void){
 //比較文字列取得 32+1文字まで
@@ -371,6 +227,7 @@ void dirlist(void){
 ////  Serial.print(c_name[lp1],HEX);
   }
 ////  Serial.println("");
+////  Serial.println(c_name);
 //
   File file = SD.open( "/" );
   File entry =  file.openNextFile();
@@ -440,14 +297,12 @@ void dirlist(void){
     }else{
       br_chk=1;
     }
-////  Serial.println("TEST2");
 //FDLの結果が20件未満なら継続指示要求せずにそのまま終了
     if (!entry && cntl2 < 20 && page ==1){
       break;
     }
   }
 //処理終了指示
-////  Serial.println("TEST3");
   snd1byte(0xFF);
   snd1byte(0x00);
 }
@@ -457,15 +312,7 @@ void dirlist(void){
 boolean f_match(char *f_name,char *c_name){
   boolean flg1 = true;
   unsigned int lp1 = 0;
-//  Serial.print(f_name);
-//  Serial.print(" ");
-//  Serial.print(c_name);
-//  Serial.print(" ");
   while (lp1 <=32 && c_name[0] != 0x00 && flg1 == true){
-//  Serial.print(f_name[lp1],HEX);
-//  Serial.print("-");
-//  Serial.print(c_name[lp1+1],HEX);
-//  Serial.print(" ");
     if (upper(f_name[lp1]) != c_name[lp1+1]){
       flg1 = false;
     }
@@ -475,242 +322,70 @@ boolean f_match(char *f_name,char *c_name){
     }
   }
 //  if (flg1){
-//    Serial.println("true");
 //  }else{
-//    Serial.println("false");
 //  }
   return flg1;
 }
 
-//FILE DELETE
-void f_del(void){
-
-//ファイルネーム取得
-  for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-    f_name[lp1] = rcv1byte();
-  }
-  addmzt(f_name);
-
-//ファイルが存在しなければERROR
-  if (SD.exists(f_name) == true){
-//状態コード送信(OK)
-    snd1byte(0x00);
-
-//処理選択を受信(0:継続してDELETE 0以外:CANSEL)
-    if (rcv1byte() == 0x00){
-      if (SD.remove(f_name) == true){
-//状態コード送信(OK)
-        snd1byte(0x00);
-      }else{
-//状態コード送信(Error)
-        snd1byte(0xf1);
-      }
-    } else{
-//状態コード送信(Cansel)
-      snd1byte(0x01);
-    }
-  }else{
-//状態コード送信(Error)
-        snd1byte(0xf1);
-  }
-}
-
-//FILE RENAME
-void f_ren(void){
-
-//現ファイルネーム取得
-  for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-    f_name[lp1] = rcv1byte();
-  }
-  addmzt(f_name);
-
-//ファイルが存在しなければERROR
-  if (SD.exists(f_name) == true){
-//状態コード送信(OK)
-    snd1byte(0x00);
-
-//新ファイルネーム取得
-    for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-      new_name[lp1] = rcv1byte();
-    }
-    addmzt(new_name);
-//状態コード送信(OK)
-    snd1byte(0x00);
-
-    File file = SD.open( f_name, FILE_WRITE );
-    if( true == file ){
-      if (file.rename(new_name)){
- //状態コード送信(OK)
-         snd1byte(0x00);
-        } else {
- //状態コード送信(OK)
-          snd1byte(0xff);
-        }
-      file.close();
-    }else{
-//状態コード送信(Error)
-      snd1byte(0xf1);
-    }
-  }else{
-//状態コード送信(Error)
-      snd1byte(0xf1);
-  }
-}
-
-//FILE DUMP
-void f_dump(void){
-unsigned int br_chk =0;
-
-//ファイルネーム取得
-  for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-    f_name[lp1] = rcv1byte();
-  }
-  addmzt(f_name);
-
-//ファイルが存在しなければERROR
-  if (SD.exists(f_name) == true){
-//状態コード送信(OK)
-    snd1byte(0x00);
-
-//ファイルオープン
-    File file = SD.open( f_name, FILE_READ );
-      if( true == file ){
-//実データ送信(1画面:128Byte)
-        unsigned int f_length = file.size();
-        long lp1 = 0;
-        while (lp1 <= f_length-1){
-//画面先頭ADRSを送信
-          snd1byte(lp1 % 256);
-          snd1byte(lp1 / 256);
-          int i=0;
-//実データを送信
-          while(i<128 && lp1<=f_length-1){
-            snd1byte(file.read());
-            i++;
-            lp1++;
-          }
-//FILE ENDが128Byteに満たなかったら残りByteに0x00を送信
-          while(i<128){
-            snd1byte(0x00);
-            i++;
-          }
-//指示待ち
-          br_chk=rcv1byte();
-//BREAKならポインタをFILE ENDとする
-          if (br_chk==0xff){
-            lp1 = f_length; 
-          }
-//B:BACKを受信したらポインタを256Byte戻す。先頭画面なら0に戻してもう一度先頭画面表示
-          if (br_chk==0x42){
-            if(lp1>255){
-              if (lp1 % 128 == 0){
-                lp1 = lp1 - 256;
-              } else {
-                lp1 = lp1 - 128 - (lp1 % 128);
-              }
-              file.seek(lp1);
-            } else{
-              lp1 = 0;
-              file.seek(0);
-            }
-          }
-        }
-//FILE ENDもしくはBREAKならADRSに終了コード0FFFFHを送信
-        if (lp1 > f_length-1){
-          snd1byte(0xff);
-          snd1byte(0xff);
-        };
-        file.close();
-//状態コード送信(OK)
-        snd1byte(0x00);
-      } else {
-//状態コード送信(ERROR)
-      snd1byte(0xF1);
-    }
-  }else{
-//状態コード送信(Error)
-        snd1byte(0xf1);
-  }
-}
-
-//FILE COPY
-void f_copy(void){
-
-//現ファイルネーム取得
-  for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-    f_name[lp1] = rcv1byte();
-  }
-  addmzt(f_name);
-//ファイルが存在しなければERROR
-  if (SD.exists(f_name) == true){
-//状態コード送信(OK)
-    snd1byte(0x00);
-
-//新ファイルネーム取得
-    for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-      new_name[lp1] = rcv1byte();
-    }
-    addmzt(new_name);
-//新ファイルネームと同じファイルネームが存在すればERROR
-    if (SD.exists(new_name) == false){
-//状態コード送信(OK)
-        snd1byte(0x00);
-//ファイルオープン
-    File file_r = SD.open( f_name, FILE_READ );
-    File file_w = SD.open( new_name, FILE_WRITE );
-      if( true == file_r ){
-//実データコピー
-        unsigned int f_length = file_r.size();
-        long lp1 = 0;
-        while (lp1 <= f_length-1){
-          int i=0;
-          while(i<=255 && lp1<=f_length-1){
-            s_data[i]=file_r.read();
-            i++;
-            lp1++;
-          }
-          file_w.write(s_data,i);
-        }
-        file_w.close();
-        file_r.close();
-//状態コード送信(OK)
-        snd1byte(0x00);
-      }else{
-//状態コード送信(Error)
-      snd1byte(0xf1);
-    }
-      }else{
-//状態コード送信(Error)
-        snd1byte(0xf3);
-    }
-  }else{
-//状態コード送信(Error)
-      snd1byte(0xf1);
-  }
-}
-
-//91hで0436H MONITOR ライト インフォメーション代替処理
+//91hでMSHED ライト インフォメーション代替処理
 void mon_whead(void){
 char m_info[130];
+unsigned int lp1,lp2;
+unsigned int n_len,e_len;
 //インフォメーションブロック受信
-  for (unsigned int lp1 = 0;lp1 < 128;lp1++){
+  for (lp1 = 0;lp1 < 32;lp1++){
     m_info[lp1] = rcv1byte();
+////  Serial.println(m_info[lp1],HEX);
   }
-//S-OS SWORDからは最後が20hのファイルネームが送られて来るため0dhを付加
-//8080用テキスト・エディタ＆アセンブラからファイルネームの後ろに20hが送られて来るため0dhに修正
-//MZ-80K側で対処
-//  int lp2 = 17;
-//  while (lp2>0 && (m_info[lp2] ==0x20 || m_info[lp2] ==0x0d)){
-//    m_info[lp2]=0x0d;
-//    lp2--;
-//  }
-//ファイルネーム取り出し
-  for (unsigned int lp1 = 0;lp1 < 17;lp1++){
-    m_name[lp1] = m_info[lp1+1];
+//ファイルネーム部分(13文字)取り出し
+  for (lp1 = 0;lp1 < 13;lp1++){
+    name1[lp1] = m_info[lp1+1];
   }
-//DOSファイルネーム用に.MZTを付加
-  addmzt(m_name);
-  m_info[16] = 0x0d;
+  name1[13]=0x00;
+  lp1=13;
+  n_len=13;
+  while((lp1!=0) && (name1[lp1-1]==0x20)){
+    n_len=lp1-1;
+    lp1--;
+  }
+
+//拡張子部分(3文字)取り出し
+  for (lp1 = 0;lp1 < 3;lp1++){
+    name2[lp1] = m_info[lp1+14];
+  }
+  name2[3]=0x00;
+
+  lp1=3;
+  e_len=3;
+  while((lp1!=0) && (name2[lp1-1]==0x20)){
+    e_len=lp1-1;
+    lp1--;
+  }
+
+  if (e_len!=0){
+//拡張子部分(3文字)がある場合の処理(filename.ext)
+    for (lp1=0;lp1<n_len;lp1++){
+      m_name[lp1]=name1[lp1];
+    }
+    m_name[n_len]=0x2e;
+    for (lp2=0;lp2<e_len;lp2++){
+      m_name[n_len+1+lp2]=name2[lp2];
+    }
+    m_name[n_len+e_len+1]=0x0d;
+    m_name[n_len+e_len+2]=0x00;
+  } else{
+//拡張子部分(3文字)が無い場合の処理(filename)
+    for (lp1=0;lp1<n_len;lp1++){
+      m_name[lp1]=name1[lp1];
+    }
+    m_name[n_len]=0x0d;
+    m_name[n_len+1]=0x00;
+  }
+
+//DOSファイルネーム用に.X1Tを付加
+  addx1t(m_name);
+////  Serial.println(m_name);
+//  m_info[16] = 0x0d;
 //ファイルが存在すればdelete
   if (SD.exists(m_name) == true){
     SD.remove(m_name);
@@ -721,23 +396,27 @@ char m_info[130];
 //状態コード送信(OK)
     snd1byte(0x00);
 //インフォメーションブロックwrite
-    for (unsigned int lp1 = 0;lp1 < 128;lp1++){
+    for (lp1 = 0;lp1 < 32;lp1++){
       file.write(m_info[lp1]);
     }
     file.close();
+////  Serial.println("Infomation Block Write OK");
   } else {
 //状態コード送信(ERROR)
     snd1byte(0xF1);
+////  Serial.println("Infomation Block Write ERROR");
   }
 }
 
-//92hで0475H MONITOR ライト データ代替処理
+//92hでMSDAT ライト データ代替処理
 void mon_wdata(void){
 //ファイルサイズ取得
   int f_length1 = rcv1byte();
   int f_length2 = rcv1byte();
 //ファイルサイズ算出
   unsigned int f_length = f_length1+f_length2*256;
+////  Serial.print("Write Size :");
+////  Serial.println(f_length,HEX);
 //ファイルオープン
   File file = SD.open( m_name, FILE_WRITE );
   if( true == file ){
@@ -755,21 +434,28 @@ void mon_wdata(void){
       file.write(s_data,i);
     }
     file.close();
+////  Serial.println("Data Block Write OK");
   } else {
 //状態コード送信(ERROR)
     snd1byte(0xF1);
+////  Serial.println("Data Block Write ERROR");
   }
 }
 
-//04D8H MONITOR リード インフォメーション代替処理
+//MLHED リード インフォメーション代替処理
 void mon_lhead(void){
 //リード データ POINTクリア
-  m_lop=128;
+  m_lop=32;
 //ファイルネーム取得
   for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-    m_name[lp1] = rcv1byte();
+    byte i_data = rcv1byte();
+    if (i_data==0x00){i_data=0x0d;}
+    m_name[lp1] = i_data;
+
+////  Serial.println(m_name[lp1],HEX);
   }
-  addmzt(m_name);
+  addx1t(m_name);
+////  Serial.println(m_name);
 //ファイルが存在しなければERROR
   if (SD.exists(m_name) == true){
     snd1byte(0x00);
@@ -777,25 +463,28 @@ void mon_lhead(void){
     File file = SD.open( m_name, FILE_READ );
     if( true == file ){
       snd1byte(0x00);
-      for (unsigned int lp1 = 0;lp1 < 128;lp1++){
+      for (unsigned int lp1 = 0;lp1 < 32;lp1++){
           byte i_data = file.read();
           snd1byte(i_data);
       }
       file.close();
       snd1byte(0x00);
+////  Serial.println("Infomation Block Read OK");
     } else {
 //状態コード送信(ERROR)
       snd1byte(0xFF);
+////  Serial.println("Infomation Block Read Error");
     }  
   } else {
 //状態コード送信(FILE NOT FIND ERROR)
     snd1byte(0xF1);
+////  Serial.println("Infomation Block Read Error");
   }
 }
 
-//04F8H MONITOR リード データ代替処理
+//MLDAT リード データ代替処理
 void mon_ldata(void){
-  addmzt(m_name);
+  addx1t(m_name);
 //ファイルが存在しなければERROR
   if (SD.exists(m_name) == true){
     snd1byte(0x00);
@@ -815,55 +504,16 @@ void mon_ldata(void){
       file.close();
       m_lop=m_lop+f_length;
       snd1byte(0x00);
+////  Serial.println("Data Block Read OK");
     } else {
 //状態コード送信(ERROR)
       snd1byte(0xFF);
+////  Serial.println("Data Block Read Error");
     }  
   } else {
 //状態コード送信(FILE NOT FIND ERROR)
     snd1byte(0xF1);
-  }
-}
-
-//BOOT処理(MZ-2000_SD専用)
-void boot(void){
-//ファイルネーム取得
-  for (unsigned int lp1 = 0;lp1 <= 32;lp1++){
-    m_name[lp1] = rcv1byte();
-  }
-////  Serial.print("m_name:");
-////  Serial.println(m_name);
-//ファイルが存在しなければERROR
-  if (SD.exists(m_name) == true){
-    snd1byte(0x00);
-//ファイルオープン
-    File file = SD.open( m_name, FILE_READ );
-    if( true == file ){
-    snd1byte(0x00);
-//ファイルサイズ送信
-      unsigned long f_length = file.size();
-      byte f_len1 = f_length / 256;
-      byte f_len2 = f_length % 256;
-      snd1byte(f_len2);
-      snd1byte(f_len1);
-////  Serial.println(f_length,HEX);
-////  Serial.println(f_len2,HEX);
-////  Serial.println(f_len1,HEX);
-
-//実データ送信
-      for (unsigned long lp1 = 1;lp1 <= f_length;lp1++){
-         byte i_data = file.read();
-         snd1byte(i_data);
-////  Serial.println(lp1,HEX);
-      }
-
-    } else {
-//状態コード送信(ERROR)
-      snd1byte(0xFF);
-    }  
-  } else {
-//状態コード送信(FILE NOT FIND ERROR)
-    snd1byte(0xF1);
+////  Serial.println("Data Block Read Error");
   }
 }
 
@@ -879,31 +529,17 @@ void loop()
   digitalWrite(PB7PIN,LOW);
   digitalWrite(FLGPIN,LOW);
 //コマンド取得待ち
-  Serial.print("cmd:");
+////  Serial.print("cmd:");
   byte cmd = rcv1byte();
-  Serial.println(cmd,HEX);
+////  Serial.println(cmd,HEX);
   if (eflg == false){
     switch(cmd) {
-//80hでSDカードにsave
-      case 0x80:
-////  Serial.println("SAVE START");
-//状態コード送信(OK)
-        snd1byte(0x00);
-        f_save();
-        break;
 //81hでSDカードからload
       case 0x81:
 ////  Serial.println("LOAD START");
 //状態コード送信(OK)
         snd1byte(0x00);
         f_load();
-        break;
-//82hで指定ファイルを0000.mztとしてリネームコピー
-      case 0x82:
-////  Serial.println("ASTART START");
-//状態コード送信(OK)
-        snd1byte(0x00);
-        astart();
         break;
 //83hでファイルリスト出力
       case 0x83:
@@ -913,77 +549,42 @@ void loop()
   // SD初期化
   if( !SD.begin(CABLESELECTPIN,8) )
   {
-    Serial.println("Failed : SD.begin");
+////    Serial.println("Failed : SD.begin");
     eflg = true;
   } else {
-    Serial.println("OK : SD.begin");
+////    Serial.println("OK : SD.begin");
     eflg = false;
   }
-  Serial.println("START");
+////  Serial.println("START");
         dirlist();
         break;
-//84hでファイルDelete
-      case 0x84:
-////  Serial.println("FILE Delete START");
-//状態コード送信(OK)
-        snd1byte(0x00);
-        f_del();
-        break;
-//85hでファイルリネーム
-      case 0x85:
-////  Serial.println("FILE Rename START");
-//状態コード送信(OK)
-        snd1byte(0x00);
-        f_ren();
-        break;
-      case 0x86:  
-//86hでファイルダンプ
-////  Serial.println("FILE Dump START");
-//状態コード送信(OK)
-        snd1byte(0x00);
-        f_dump();
-        break;
-      case 0x87:  
-//87hでファイルコピー
-////  Serial.println("FILE Copy START");
-//状態コード送信(OK)
-        snd1byte(0x00);
-        f_copy();
-        break;
       case 0x91:
-//91hで0436H MONITOR ライト インフォメーション代替処理
-////  Serial.println("0436H START");
+//91hでMSHED ライト インフォメーション代替処理
+////  Serial.println("Wright Infomation START");
 //状態コード送信(OK)
         snd1byte(0x00);
         mon_whead();
         break;
-//92hで0475H MONITOR ライト データ代替処理
+//92hでMSDAT ライト データ代替処理
       case 0x92:
-////  Serial.println("0475H START");
+////  Serial.println("Wright Data START");
 //状態コード送信(OK)
         snd1byte(0x00);
         mon_wdata();
         break;
-//93hで04D8H MONITOR リード インフォメーション代替処理
+//93hでMLHED リード インフォメーション代替処理
       case 0x93:
-////  Serial.println("04D8H START");
+////  Serial.println("Read Infomation START");
 //状態コード送信(OK)
         snd1byte(0x00);
         mon_lhead();
         break;
-//94hで04F8H MONITOR リード データ代替処理
+//94hでMLDAT リード データ代替処理
       case 0x94:
-////  Serial.println("04F8H START");
+////  Serial.println("Read Data START");
 //状態コード送信(OK)
         snd1byte(0x00);
         mon_ldata();
-        break;
-//95hでBOOT LOAD(MZ-2000_SD専用)
-      case 0x95:
-////  Serial.println("BOOT LOAD START");
-//状態コード送信(OK)
-        snd1byte(0x00);
-        boot();
         break;
       default:
 //状態コード送信(CMD ERROR)
